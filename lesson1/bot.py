f703dc67c019e15ae2e1ecde8dca7ca6bb9446c9
herrenot <herrenor@gmail.com>
2017-09-02 18:21:16 +0300

    booot

---
+++ lesson1/bot.py
@@ -0,0 +1,29 @@
+
+from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
+
+import logging
+logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    filename='bot.log'
+                    )
+
+def main():
+    updater = Updater("389750746:AAFXpEl5WcZAQsFL3M1u5hrgRcsaZPvZF0w")
+    dp = updater.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    updater.start_polling()
+    updater.idle()
+       
+
+def greet_user(bot, update):
+    text = 'Вызван /start'
+    print(text)
+    update.message.reply_text(text)
+
+def talk_to_me(bot, update):
+    user_text = update.message.text 
+    print(user_text)
+    update.message.reply_text(user_text)    
+
+main()
